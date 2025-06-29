aebd669136a4618411f336ce52ead04b7b76a2b5
bmax911 <binhdq1996@gmail.com>
2025-06-29 09:50:30 +0700

    Docs: update

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'templates/index.html',
-        pricing: 'templates/pricing.html',
-        signup: 'templates/signup.html'
-      }
-    }
-  }
 })
